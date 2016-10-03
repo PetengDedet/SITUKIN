d9103bfb8384cec565929333be171c6d170bf58e
PetengDedet <pueteng.dedet@gmail.com>
2016-10-03 14:18:02 +0700

    add master template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.header')
+
+@yield('css')
+
+@include('layouts.top-nav')
+@include('layouts.sidebar')
+
+@yield('content')
+
+@include('layouts.footer')
+
+@yield('js')
+
+</body>
+</html>
